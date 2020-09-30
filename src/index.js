01f6e1f281cd73799a98f9b12d7d811bdc465c5a
Anton Omelchuk <anton.omelchuk@wisent-media.by>
2020-09-30 23:59:05 +0300

    feat: completed typical problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 
 exports.min = function min (array) {
-  return 0;
+	return array && array.length ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  	return array && array.length ? Math.max.apply(null, array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	const sum = array ? array.reduce((a, b) => a + b, 0) : 0;
+	const avg =  array ? (sum / array.length) : 0;
+	return array && array.length ? avg : 0;
 }
